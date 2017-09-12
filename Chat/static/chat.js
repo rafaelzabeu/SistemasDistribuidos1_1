8e0a96f424db96d48e1c9986f31e3fe646e7bd0b
mvpstds <mvpstds@gmail.com>
2017-09-11 22:47:50 -0300

    Added name input

--- Chat/static/chat.js
+++ Chat/static/chat.js
@@ -2,12 +2,19 @@ $(document).ready(function() {
     var namespace = '/chat'
 
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
-
+	//gets the name value
+	var username =  document.getElementsById('user_name').value;
+    var isName = false;
     //where the messages are displayed
     var textArea = $('#textArea');
     //box where the user writes their messages
     var emmitDataInput = $('#emit_data');
-
+	
+	//hides the chat elements
+	document.getElementsById('textArea').style.visibility = 'hidden';
+	document.getElementsById('input').style.visibility = 'hidden';
+	document.getElementsById('submit').style.visibility = 'hidden';
+	
     socket.on('connect', function() {
         //sends the naming event to the server. TODO: Implement name input for the user
         socket.emit('sync_name', {name: 'coisa'});
@@ -35,5 +42,24 @@ $(document).ready(function() {
         socket.emit('disconnect_request');
         return false;
     });
+	
+	//Verifies if the user has a name
+	if (nome.length > 0)
+    {
+        isNome = true;
+    }
+    else
+    {
+        alert("O campo nome esta vazio!");
+    }
+	
+	//Shows chat elements 
+	if (isNome == true)
+	{
+		document.getElementsById('user_name').style.visibility = 'hidden';
+		document.getElementsById('textArea').style.visibility = 'visible';
+		document.getElementsById('input').style.visibility = 'visible';
+		document.getElementsById('submit').style.visibility = 'visible';
+	}
 
 });
